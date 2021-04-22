572d777b5fa59a529882562931ebece0f3172383
manos89 <mvahlas@gmail.com>
2021-04-22 08:20:59 +0300

    added extentions in client.py

--- esdm_pav_client/cli/client.py
+++ esdm_pav_client/cli/client.py
@@ -1,6 +1,9 @@
 import click
 import sys
-
+# import os
+#
+# previous_dir = os.path.dirname(os.getcwd())
+# sys.path.insert(0, os.path.dirname(previous_dir))
 sys.path.insert(0, "..")
 from esdm_pav_client import Workflow
 
@@ -12,10 +15,20 @@ from esdm_pav_client import Workflow
 @click.option(
     "-S", "--server", help="ESDM PAV Runtime address", default="127.0.0.1"
 )
-@click.option("-P", "--port", help="ESDM PAV Runtime port", default="11732")
+@click.option("-P", "--port", help="ESDM PAV Runtime port", default="11732"
+)
+@click.option(
+    "-m", "--monitor", is_flag=True, help="Will display a graph of the workflow"
+)
+@click.option(
+    "-a", "--async_mode", is_flag=True, help="Will change the exec_mode to async"
+)
+@click.option(
+    "-c", "--cancel", is_flag=True, help="Will cancel the workflow. It only works in the async_mode"
+)
 @click.argument("workflow")
 @click.argument("workflow_args", nargs=-1, type=click.UNPROCESSED)
-def run(verbose, server, port, workflow, workflow_args):
+def run(verbose, server, port, monitor, async_mode, cancel, workflow, workflow_args):
     """Command Line Interface to run an ESDM PAV experiment WORKFLOW\n
        Example: client.py experiment.json 1 2"""
 
@@ -43,9 +56,18 @@ def run(verbose, server, port, workflow, workflow_args):
     if verbose:
         click.echo("Reading the workflow")
     w1 = Workflow.load(workflow)
+
     if verbose:
         click.echo("Submitting the workflow")
+    if async_mode:
+        w1.exec_mode = "async"
     w1.submit(server=server, port=port, *args)
+    if async_mode:
+        click.echo("Workflow ID: {0}".format(str(w1.workflow_id)))
+        if cancel:
+            w1.cancel()
+    if monitor:
+        w1.monitor()
 
 
 if __name__ == "__main__":
